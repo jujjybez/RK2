04abddcf878b2b198739d2f722978f1ff3973cd7
jujjybez <144797201+jujjybez@users.noreply.github.com>
2024-06-03 11:35:19 +0300

    Test

--- test.cpp
+++ test.cpp
@@ -4,17 +4,17 @@
 #include "Shape.h"
 #include "OpenGLDrawStrategy.h"
 
-TEST(ShapeTest, CircleArea) {
+TEST(Shape_Test, CircleArea) {
     Circle circle(2.0);
     EXPECT_DOUBLE_EQ(circle.radius(), 2);
 }
 
-TEST(ShapeTest, SquareArea) {
+TEST(Shape_Test, SquareArea) {
     Square square(3.0);
     EXPECT_DOUBLE_EQ(square.side(), 3.0);
 }
 
-TEST(ShapeTest, SquareArea) {
+TEST(Shape_Test, SquareArea) {
     Square square(4.0);
     EXPECT_DOUBLE_EQ(square.side(), 4);
 }
